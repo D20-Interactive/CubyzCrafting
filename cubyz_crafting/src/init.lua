271dc7a0b92fde78214bbd313d89d2fc14ba61cf
Aditya Kumar <adityaadi111203@gmail.com>
2026-02-27 21:44:59 +0530

    First test

---
+++ cubyz_crafting/src/init.lua
@@ -0,0 +1,50 @@
+print("This file will be run at load time!")
+
+core.register_node("cubyzcrafting:workbench", {
+    description = "Workbench to make tools",
+    tiles = {
+        "crafting_workbench_top.png",
+        "",
+        "crafting_workbench_side.png",
+        "crafting_workbench_front.png",
+    },
+    paramtype2 = "facedir",
+    groups = {choppy = 1, workbench = 1},
+    on_construct = function(pos)
+        local meta = core.get_meta(pos)
+        meta:set_string("infotext", "Workbench")
+    end,
+    on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+        -- Open the crafting UI
+        core.show_formspec(player:get_player_name(), "cubyzcrafting_workbench:form", 
+            "size[8,8]" ..
+            "label[1,1;Crafting Menu]" ..
+            "button[1,5;2,1;craft;Craft]"
+        )
+    end,
+})
+
+core.register_craft({
+    type = "shaped",
+    output = "cubyzcrafting:workbench",
+    recipe = {
+        {"", "default:wood", ""},
+        {"default:wood", "default:wood", "default:wood"},
+        {"", "default:wood", ""},
+    },
+})
+
+core.register_on_player_receive_fields(function(player, formname, fields)
+    if formname == "cubyzcrafting_workbench:form" then
+        if fields.craft then
+            local inv = player:get_inventory()
+            if inv:contains_item("main", "default:wood 5") then
+                inv:remove_item("main", "default:wood 5")
+                inv:add_item("main", "cubyzcrafting:workbench")
+                core.chat_send_player(player:get_player_name(), "Crafted 1 Workbench!")
+            else
+                core.chat_send_player(player:get_player_name(), "Not enough wood planks!")
+            end
+        end
+    end
+end)
